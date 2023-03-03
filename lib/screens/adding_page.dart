f81143a656de9c42a7a42526fd2873a2d79f42b4
shakhzod_bakhromov <shakhzod_bakhromov@mail.ru>
2023-03-03 13:10:07 +0000

    adding page

--- lib/screens/adding_page.dart
+++ lib/screens/adding_page.dart
@@ -1,4 +1,8 @@
+import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../colors/colors.dart';
 
 class AddingPage extends StatefulWidget {
   const AddingPage({Key? key}) : super(key: key);
@@ -10,6 +14,79 @@ class AddingPage extends StatefulWidget {
 class _AddingPageState extends State<AddingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: bkground,
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Text("Add a new task", style: TextStyle(fontSize: 20,color: bottomNav,fontWeight: FontWeight.w500),),
+            Spacer(),
+            Icon(Icons.cancel,color: bottomNav,)
+          ],
+        ),
+        elevation: 0,
+        backgroundColor: bkground,
+      ),
+      body:Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        margin: EdgeInsets.only(left: 15,right: 15),
+
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15,vertical: 15),
+              margin: EdgeInsets.only(top: 25,bottom: 30),
+              decoration: BoxDecoration(
+                  color: bottomNav,
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              child: Row(
+                children: [
+                  Text("Choose a task from your saved tasks list", style: TextStyle(fontSize: 14, color: Colors.white),),
+                  Spacer(),
+                  Icon(Icons.double_arrow,color: Colors.white,)
+                ],
+              ),
+            ),
+            Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Select category*", style: TextStyle(fontSize: 14,color: Colors.orange),)),
+            Container(
+
+              padding: EdgeInsets.only(left: 0),
+              margin: EdgeInsets.only(top: 25,bottom: 30),
+              decoration: BoxDecoration(
+                  color: Color(0xFFf7cfa6),
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              child: DefaultTabController(
+                length: 4,
+                child: Row(
+                  children: <Widget>[
+                  ButtonsTabBar(
+                    backgroundColor: Colors.orange,
+                    unselectedBackgroundColor: Color(0xFFf7cfa6),
+                    unselectedBorderColor:  Color(0xFFf7cfa6),
+                    unselectedLabelStyle: TextStyle(color: Colors.orange),
+                    labelStyle:
+                    TextStyle(color: Colors.white,fontSize: 20, fontWeight: FontWeight.bold),
+                    tabs: [
+                      Tab(text: "Islam"),
+                      Tab(text: "Family"),
+                      Tab(text: "Work"),
+                      Tab(text: "Personal"),
+
+                  ],
+                ),
+              ]
+                ),
+            )
+
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
