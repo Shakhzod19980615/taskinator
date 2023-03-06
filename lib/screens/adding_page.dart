0b81aa97ddc27faa43de30c17d5478f31efdf664
shakhzod_bakhromov <shakhzod_bakhromov@mail.ru>
2023-03-06 11:02:44 +0000

    tab bar

--- lib/screens/adding_page.dart
+++ lib/screens/adding_page.dart
@@ -12,6 +12,8 @@ class AddingPage extends StatefulWidget {
 }
 
 class _AddingPageState extends State<AddingPage> {
+  final titleController = TextEditingController();
+  final titleDescriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,64 +29,99 @@ class _AddingPageState extends State<AddingPage> {
         elevation: 0,
         backgroundColor: bkground,
       ),
-      body:Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        margin: EdgeInsets.only(left: 15,right: 15),
+      body:SingleChildScrollView(
 
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15,vertical: 15),
-              margin: EdgeInsets.only(top: 25,bottom: 30),
-              decoration: BoxDecoration(
-                  color: bottomNav,
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              child: Row(
-                children: [
-                  Text("Choose a task from your saved tasks list", style: TextStyle(fontSize: 14, color: Colors.white),),
-                  Spacer(),
-                  Icon(Icons.double_arrow,color: Colors.white,)
-                ],
-              ),
-            ),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Select category*", style: TextStyle(fontSize: 14,color: Colors.orange),)),
-            Container(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          margin: EdgeInsets.only(left: 15,right: 15),
 
-              padding: EdgeInsets.only(left: 0),
-              margin: EdgeInsets.only(top: 25,bottom: 30),
-              decoration: BoxDecoration(
-                  color: Color(0xFFf7cfa6),
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              child: DefaultTabController(
-                length: 4,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15,vertical: 15),
+                margin: EdgeInsets.only(top: 25,bottom: 30),
+                decoration: BoxDecoration(
+                    color: bottomNav,
+                    borderRadius: BorderRadius.circular(10)
+                ),
                 child: Row(
-                  children: <Widget>[
-                  ButtonsTabBar(
-                    backgroundColor: Colors.orange,
-                    unselectedBackgroundColor: Color(0xFFf7cfa6),
-                    unselectedBorderColor:  Color(0xFFf7cfa6),
-                    unselectedLabelStyle: TextStyle(color: Colors.orange),
-                    labelStyle:
-                    TextStyle(color: Colors.white,fontSize: 20, fontWeight: FontWeight.bold),
-                    tabs: [
-                      Tab(text: "Islam"),
-                      Tab(text: "Family"),
-                      Tab(text: "Work"),
-                      Tab(text: "Personal"),
-
+                  children: [
+                    Text("Choose a task from your saved tasks list", style: TextStyle(fontSize: 14, color: Colors.white),),
+                    Spacer(),
+                    Icon(Icons.double_arrow,color: Colors.white,)
                   ],
                 ),
-              ]
-                ),
-            )
+              ),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Select category*", style: TextStyle(fontSize: 14,color: Colors.orange),)),
+              Container(
 
-            )
-          ],
+                padding: EdgeInsets.only(left: 20),
+                margin: EdgeInsets.only(top: 25,bottom: 30),
+                decoration: BoxDecoration(
+                    color: Color(0xFFf7cfa6),
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: DefaultTabController(
+                  length: 4,
+                  child: Row(
+                    children: <Widget>[
+                    ButtonsTabBar(
+                      backgroundColor: Colors.orange,
+                      unselectedBackgroundColor: Color(0xFFf7cfa6),
+                      unselectedBorderColor:  Color(0xFFf7cfa6),
+                      unselectedLabelStyle: TextStyle(color: Colors.orange),
+                      labelStyle:
+                      TextStyle(color: Colors.white,fontSize: 18, fontWeight: FontWeight.bold),
+                      tabs: [
+                        Tab(text: "Islam"),
+                        Tab(text: "Family"),
+                        Tab(text: "Work"),
+                        Tab(text: "Personal"),
+
+                    ],
+                  ),
+                ]
+                  ),
+              )
+
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                child: TextFormField(
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.orange )
+                    ),
+                    disabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: CupertinoColors.inactiveGray)
+                    ),
+                    labelText: 'Task title*',
+                    labelStyle: TextStyle(color:  Colors.orange)
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                child: TextFormField(
+                  controller: titleDescriptionController,
+                  decoration: const InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.orange )
+                      ),
+                      disabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: CupertinoColors.inactiveGray)
+                      ),
+                      labelText: 'Task description',
+                      labelStyle: TextStyle(color:  Colors.orange)
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
